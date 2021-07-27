54eb6d15260d61c8c294522f47529d4c28a91070
ray-vega <jesus_vega_r@outlook.es>
2021-07-27 15:58:40 -0500

    Created Develop

--- posts/apps.py
+++
@@ -1,9 +0,0 @@
-"""Posts application module"""
-from django.apps import AppConfig
-
-
-class PostsConfig(AppConfig):
-    """Posts application settings"""
-    
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'posts'
